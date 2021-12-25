5174b60199c92fce822111516fbf11a1d5d33ea8
bahar yilmaz <bahar.yilmaz@teamdefinex.com>
2021-12-25 19:15:53 +0300

    LazySingletonPattern Created

---
+++ DesignPattern/src/LazySingletonPattern.java
@@ -0,0 +1,22 @@
+
+public class LazySingletonPattern {
+	
+public static LazySingletonPattern singleton;
+	private static int count;
+	private String name;
+	
+	private LazySingletonPattern() {
+		count++;
+		name= "LazySingletonPattern" + count ;
+	}
+	public static LazySingletonPattern getInstance() {
+		if (singleton == null) {
+			singleton =new LazySingletonPattern();
+		}
+		return singleton;
+	}
+	public void printName () {
+		System.out.println(name);
+	}
+
+}
