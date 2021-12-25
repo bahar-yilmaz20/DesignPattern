d0d7ba1114d0c8e88e65db1d344cd9aafbcdee2e
bahar yilmaz <bahar.yilmaz@teamdefinex.com>
2021-12-25 19:32:18 +0300

    ThreadedLazySingleton
    You can see that Threaded Lazy Singleton does not work right

---
+++ DesignPattern/src/ThreadedLazySingletonClient.java
@@ -0,0 +1,14 @@
+
+public class ThreadedLazySingletonClient extends Thread{
+	public static void main(String[] args) {
+		for(int i=0; i<100; i++)
+		{
+			new ThreadedLazySingletonClient().start();
+		}
+	}
+	
+	public void run() {
+		ThreadedLazySingleton ls= ThreadedLazySingleton.getInstance();
+		ls.printName();
+	}
+}
