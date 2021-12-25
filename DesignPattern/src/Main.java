a76916291129a87f2ad1cbaec7f83800f931d127
bahar yilmaz <bahar.yilmaz@teamdefinex.com>
2021-12-25 19:15:34 +0300

    SingletonPatterCreated

--- DesignPattern/src/Main.java
+++ DesignPattern/src/Main.java
@@ -1,9 +1,31 @@
+import java.time.LocalDateTime;
+
+import javax.print.attribute.standard.DateTimeAtCreation;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		// For measuring how many obje was created
+		for (int i = 0; i < 10; i++) {
+			SingletonPattern s = SingletonPattern.getInstance();
+			s.printName();		
+		}
+		
+		// For measuring that same object that created in the different time
+		SingletonPattern s1= SingletonPattern.getInstance();
+		System.out.println("\nHashcode: "+s1.hashCode());
+		
+		SingletonPattern s2= SingletonPattern.getInstance();
+		System.out.println("\nHashcode: "+s2.hashCode());
+		
+		
+		
+		
+		
+		System.console();
 	}
 
 }
